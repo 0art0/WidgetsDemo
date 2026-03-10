41b5dacd8ac6c2a0df7be4649806ea07b6e12723
0art0 <18333981+0art0@users.noreply.github.com>
2026-03-10 23:09:53 +0000

    Bump toolchain

--- lakefile.lean
+++ lakefile.lean
@@ -4,7 +4,7 @@ open Lake DSL System
 
 package WidgetsDemo
 
-require "leanprover-community" / "proofwidgets" @ git "v0.0.77"
+require "leanprover-community" / "proofwidgets" @ git "v0.0.92"
 
 require "0art0" / "lean-slides"
 
